f64890a706ac73f861aa5846183111c1fdff32e9
deepchocolate <andreas@systerdyster.se>
2025-08-19 13:56:42 +0200

    Added assertions

--- tests/testthat/test_r2wrap.R
+++ tests/testthat/test_r2wrap.R
@@ -1,4 +1,8 @@
 source('helper.R')
+# "prevalence" is set in helper.R
+# Sequences are tested by creating an incremented vector
+prevalenceVector <- c(prevalence, prevalence + seq(0.01,0.03, 0.01))
+
 # Compare with DescTools::PseudoR2 and r2redux::cc_trf
 test_that('Testing linear model on a y=1/0', {
   m <- glm(yth~x, data=dtaSamp)
@@ -44,16 +48,22 @@ test_that('Testing binomial model on a y=1/0', {
   expect_equal(mP@cases, nCas)
   expect_equal(mP@controls, nCont)
 
-  # Nagelkerke
+  ### Nagelkerke
   r2 <- NagelkerkeR2(m)$R2
   expect_equal(R2.Nagelkerke(mP), r2)
-  # Scale liability
+
+  ### Scale liability
   # We calculate the observed scale R2 manually as this is not done by r2redux::cc_trf to get liability scale R2
   r2 <- var(predict(m, type='response'))/(prevalenceSample*(1-prevalenceSample))
   expect_equal(R2.scaleObserved(mP), r2)
   # This function takes a SD of R2 aswell, but this is not relevant for the calculation of libability scale point estimate R2
   r2 <- cc_trf(r2, 0.01, K=prevalence, P=prevalenceSample)$R2l
-  expect_equal(R2.scaleLiability(mP, prevalence), r2)
+  r2Liab <- R2.scaleLiability(mP, prevalence)
+  expect_equal(r2Liab, r2)
+  # Test using a vector
+  r2LiabVec <- R2.scaleLiability(mP, prevalenceVector)
+  expect_length(r2LiabVec, length(prevalenceVector))
+  expect_equal(r2LiabVec[1], r2)
   # This is just testing using a different coefficient (the error term)
   m <- glm(yth~x + z, data=dtaSamp, family=binomial)
   mP <- R2Wrap(m, comparison=~.-z)
